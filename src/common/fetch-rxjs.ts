c45c150de80cfe71cf64850754a644bca86baef9
Dang <dttung1412@gmail.com>
2018-09-15 23:19:05 +0700

    feat: use fetch-rxjs

--- src/common/fetch-rxjs.ts
+++ src/common/fetch-rxjs.ts
@@ -1,10 +1,6 @@
 import { Observable, from } from 'rxjs';
 import fetch from '../lib/isomorphic-fetch';
 
-export function isNode(): boolean {
-  return typeof window === 'undefined' && !(typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope);
-}
-
 // note: this stream is hot (fetch function run without subscribe)
 export function fetchRxjs<T>(url: string, options?: {}): Observable<T> {
   // patch the error in browser running webpack built js: change the context of fetch to not window object
--- src/common/index.ts
+++ src/common/index.ts
@@ -1,3 +1,3 @@
-export * from './functions';
+export * from './fetch-rxjs';
 export * from './pubnub-rxjs';
 export * from './websocket-rxjs';
