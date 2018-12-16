1d9f3668cb3a101429c04e765c299e7428917742
unknown <dttung1412@gmail.com>
2018-12-17 00:07:28 +0900

    chore: remove comment

--- src/exchanges/bitfinex/bitfinex-common.ts
+++ src/exchanges/bitfinex/bitfinex-common.ts
@@ -1,22 +1,6 @@
-// import { WebsocketSubOrUnSubRequest, WebsocketRequestResponse } from './bitfinex-common.types';
-
 export const wsEndpoint = 'wss://api.bitfinex.com/ws/2';
 export const apiEndpoint = 'https://api.bitfinex.com/v2';
 
 export function getSymbol(pair: string): string {
   return `t${pair.replace('_', '').toUpperCase()}`;
 }
-
-// export function getKey(subscribeObject: WebsocketSubOrUnSubRequest | WebsocketRequestResponse): string {
-//   return (
-//     subscribeObject.channel +
-//     // (ticker, orderbook)
-//     (subscribeObject.symbol || '') +
-//     // (orderbook)
-//     (subscribeObject.prec || '') +
-//     (subscribeObject.freq || '') +
-//     (subscribeObject.len || '') +
-//     // (candle)
-//     (subscribeObject.key || '')
-//   );
-// }
