6c48663cbefebd79cbae27d606f7dfab8b52d61d
Dang <dttung1412@gmail.com>
2019-02-10 15:26:42 +0900

    feat(bitfinex-ws): more declarative implementation

--- src/exchanges/bitfinex/websocket/bitfinex-websocket.spec.ts
+++ src/exchanges/bitfinex/websocket/bitfinex-websocket.spec.ts
@@ -1,6 +1,6 @@
 import { BitfinexWebsocket } from './bitfinex-websocket';
 import { MOCK_SOCKET } from './test-helpers';
-import { BitfinexRawTicker } from './bitfinex-websocket.type';
+import { BitfinexRawTickerI } from './bitfinex-websocket.type';
 import { take } from 'rxjs/operators';
 
 const bitfinexWebsocket = new BitfinexWebsocket('', () => MOCK_SOCKET as any);
@@ -8,14 +8,14 @@ const bitfinexWebsocket = new BitfinexWebsocket('', () => MOCK_SOCKET as any);
 describe('bitfinexWebsocket', () => {
   it('should subscribe', async () => {
     const raw = await bitfinexWebsocket
-      .subscribe<BitfinexRawTicker>({channel: 'ticker',symbol: 'tBTCUSD'}).pipe(take(1)).toPromise();
+      .subscribe<BitfinexRawTickerI>({channel: 'ticker',symbol: 'tBTCUSD'}).pipe(take(1)).toPromise();
 
     expect(raw).toEqual([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
   });
 
   it('should unsubscribe', (done) => {
     bitfinexWebsocket
-      .subscribe<BitfinexRawTicker>({ channel: 'ticker', symbol: 'tBTCUSD' }).subscribe(
+      .subscribe<BitfinexRawTickerI>({ channel: 'ticker', symbol: 'tBTCUSD' }).subscribe(
         () => {},
         (e) => {},
         () => { done() },
--- src/exchanges/bitfinex/websocket/bitfinex-websocket.type.ts
+++ src/exchanges/bitfinex/websocket/bitfinex-websocket.type.ts
@@ -1,8 +1,51 @@
+export interface WebsocketRequestBaseI {
+  channel: string;
+
+  // ex: tBTCUSD (ticker, orderbook)
+  symbol?: string;
+
+  // (orderbook)
+  // level of price aggregation (P0, P1, P2, P3)
+  // default: P0
+  prec?: string;
+
+  // (orderbook)
+  // Frequency of updates(F0, F1).
+  // F0 = realtime / F1=2sec.
+  freq?: string;
+
+  // (orderbook)
+  len?: string;
+
+  // (candles)
+  key?: string;
+}
+
+export interface WebsocketSubscribeI extends WebsocketRequestBaseI {
+  event: 'subscribe';
+}
+
+export interface WebsocketUnSubscribeI {
+  event: 'unsubscribe';
+  chanId: number;
+}
+
+export interface WebsocketResponseI extends WebsocketRequestBaseI {
+  event: 'subscribed' | 'unsubscribed';
+  chanId: number;
+  // ex: BTCUSD
+  pair?: string;
+}
+
+export type WebsocketDataI = [number, any] | [number, string, any];
+
+export type WebsocketMessageI = WebsocketResponseI | WebsocketDataI;
+
 /**
  * Ticker type
  */
 
-export type BitfinexRawTicker = [
+export type BitfinexRawTickerI = [
   number, // bid 8219.2,
   number, // bid size 51.73033331,
   number, // ask 8220,
