7894f8d9281f9fb5e20039ce2d79d13188a33c4b
Dang <dttung1412@gmail.com>
2019-06-01 08:35:56 +0900

    clean

--- src/exchanges/binance/api-key-test.ts
+++
@@ -1,4 +0,0 @@
-export const apiKeys = {
-  key: 'arDHXeFG00leDJSzWpPQMdnALnoKkdBTzqhqxB5ci3OnVFxu74O8MA3ntjZcF04r',
-  secret: 'VyeDbKbpKQiBfXxmsVORfDfhrwIqwULf6VNfTUbOybx1JibXG15A1Nlc8894RJq1',
-};
