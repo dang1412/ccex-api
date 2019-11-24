710a44d8d5b2cc9069aef89bb1ae143702cbf93b
dang1412 <dttung1412@gmail.com>
2019-11-24 23:14:54 +0900

    chore: remove old

--- src/exchanges/binance/ticker/test-helpers/mock-websocket.ts
+++ src/exchanges/binance/ticker/test-helpers/mock-websocket.ts
@@ -1,35 +1,5 @@
 import { WebsocketRxJsMock } from '../../../../common';
 
-// const MOCK_TICKER_DATA = [
-//   {
-//     a: '100',
-//     b: '99',
-//     l: '98',
-//     h: '101',
-//     c: '100',
-//     v: '25.5',
-//     p: '10',
-//     P: '5.5',
-//     E: '2019-01-01',
-//     o: '98.5',
-//     x: '90',
-//   },
-//   {
-//     a: '101',
-//     b: '99',
-//     l: '98',
-//     h: '101',
-//     c: '100',
-//     v: '25.5',
-//     p: '10',
-//     P: '5.5',
-//     E: '2019-01-02',
-//     o: '98.5',
-//     x: '90',
-//   },
-//   'stop',
-// ];
-
 export const MOCK_SOCKET = new WebsocketRxJsMock<any>({
   '{"method":"SUBSCRIBE","params":["btcusdt@ticker"],"id":1}': [
     {
